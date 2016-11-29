36f3461972eaac87037e34616021e415877faa51
krishnakartik1 <krishnakartik1@gmail.com>
2016-11-29 22:17:18 +0530

    extra data

--- appleOrange.py
+++ appleOrange.py
@@ -1,5 +1,5 @@
 from sklearn import tree
-features=[[140,1],[130,1],[150,0],[170,0]] #smooth =1,bumpy=0
+features=[[140,1],[130,1],[120,1],[150,0],[170,0]] #smooth =1,bumpy=0
 labels = [0,0,1,1] #apple = 0,orange=1
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(features,labels)
