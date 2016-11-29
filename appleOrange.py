25a3e02f2c275a2226b8c88a5d1a5cab9684c82d
krishnakartik1 <krishnakartik1@gmail.com>
2016-11-29 22:11:09 +0530

    second commit

--- appleOrange.py
+++ appleOrange.py
@@ -3,4 +3,5 @@ features=[[140,1],[130,1],[150,0],[170,0]] #smooth =1,bumpy=0
 labels = [0,0,1,1] #apple = 0,orange=1
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(features,labels)
-print clf.predict([[50,0]])
+print clf.predict([[50,0]]) 
+#addition
